58688d4fb52eef8e3b6838a234459f7da374413f
sllq <axekote21@gmail.com>
2020-03-25 23:15:03 +0500

    4th 5th 6th

--- js/metmash.js
+++ js/metmash.js
@@ -1,9 +1,7 @@
 document.writeln('<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>');
 document.writeln('<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>');
-
 // $('head').append('<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="crossorigin="anonymous"></script>');
-// $('head').append('<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>');
-// $('head').append('<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>');
+
 $( document ).ready(function() {
   $('#tabs').slick({
       asNavFor: '#caption',
@@ -27,5 +25,5 @@ $( document ).ready(function() {
     speed: 200,
     fade: true,
     arrows:false,
-  })
+  });
 });
